9e44d613a7ca94dc0cc9f2f4b0fad833c6a3445f
GCQ2119216031 <91397531+GCQ2119216031@users.noreply.github.com>
2021-09-27 19:59:50 +0800

    Add files via upload

---
+++ src/drug_top5_resluts.m
@@ -0,0 +1,27 @@
+load PREDICT;
+inter = predictAdMatdgc;
+interIndex = find(inter);
+Score_Mat = Score_matrix;
+Score_Mat(interIndex) = 0;
+Mat = Score_Mat';
+[row,col] = size(Mat);
+[~,index] = sort(Mat,2,'descend');
+top = 5;
+new_row = row * top;
+predictInter = index(:,1:top); 
+results = zeros(new_row,2);
+results = string(results);
+i = 1;
+for drg_j = 1:row
+    for a = 0:top-1
+        results(i+a,1) = drugname(drg_j);
+    end
+    i = i + a + 1;
+    drugname(drg_j)
+end
+for drg_i = 1 : row
+    for dis_i = 1 : top
+        results(((drg_i-1)*5 + dis_i), 2 ) = disname(predictInter(drg_i,dis_i));
+    end
+end
+
