07ef00053fa0c0d30f292ab8466e767d99f2b5d9
GCQ2119216031 <91397531+GCQ2119216031@users.noreply.github.com>
2021-12-10 19:06:28 +0800

    Add files via upload

---
+++ src/Curve_Dis_SingleSim.m
@@ -0,0 +1,28 @@
+
+%% ROC curve
+load Gragh_Dis_singleSim
+plot(SKF_FPR , SKF_TPR,'Color','#FF4500','LineWidth',1.5,'LineStyle','-');
+hold on
+plot(InterSim_FPR ,InterSim_TPR,'Color','#4169E1','LineWidth',1.5,'LineStyle','-');
+hold on
+plot(SemSim_FPR ,SemSim_TPR,'Color','#00C957','LineWidth',1.5,'LineStyle','-');
+grid on
+h2 = legend({'SKF AUROC = 0.828','Association Similarity AUROC = 0.707','Semantic Similarity AUROC = 0.741'});
+set(h2,'FontName','Times New Roman','FontSize',10)
+title('Disease Subspace ROC Curve','FontName','Times New Roman','FontSize',12)
+xlabel('False Positive Rate','FontName','Times New Roman','FontSize',12)
+ylabel('True Positive Rate','FontName','Times New Roman','FontSize',12)
+
+%% PR curve
+load Gragh_Dis_singleSim
+plot(SKF_TPR ,SKF_Precision,'Color','#FF4500','LineWidth',1.5,'LineStyle','-');
+hold on
+plot(InterSim_TPR ,InterSim_Precision,'Color','#4169E1','LineWidth',1.5,'LineStyle','-');
+hold on
+plot(SemSim_TPR ,SemSim_Precision,'Color','#00C957','LineWidth',1.5,'LineStyle','-');
+grid on
+h2 = legend({'SKF AUPR = 0.360','Association Similarity AUPR = 0.215','Semantic Similarity AUPR = 0.106'});
+set(h2,'FontName','Times New Roman','FontSize',10)
+title('Disease Subspace PR Curve','FontName','Times New Roman','FontSize',12)
+xlabel('Recall','FontName','Times New Roman','FontSize',12)
+ylabel('Precision','FontName','Times New Roman','FontSize',12)
